a0bba4496f2ca7d0ffc562d700c9d60f7e07bb37
Ульяна Ивашкова <uliana.ivashkova@gmail.com>
2022-10-08 00:38:47 +0300

    Add year check for leap year

---
+++ DateTime.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  if ((year mod 4 = 0) and (year mod 100 <> 0)) or ((year mod 100 = 0) and (year mod 400 = 0)) then
+    Println('Високосный год')
+  else
+    Println('Невисокосный год');
+end.
